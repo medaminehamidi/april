c22833ba0410254bd1ed2ee44822fc83ce52be00
amine <amine@alphorm.com>
2021-09-14 16:33:22 +0100

    edit ui and add go to top button

--- dist/js/slideshow.js
+++ dist/js/slideshow.js
@@ -27,3 +27,22 @@ function carousel2() {
     x[slideIndex2 - 1].style.display = "block";
     setTimeout(carousel2, 3000); // Change image every 2 seconds
 }
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
